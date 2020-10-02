bb2ad0463f51cc0e212cd7bf6cc1bca3f68dcd35
bibekebib <42284727+bibekebib@users.noreply.github.com>
2020-10-02 14:38:52 +0545

    dataset grabbing part done

---
+++ ok.py
@@ -0,0 +1,14 @@
+import json
+
+
+def save_data(title, data):
+    with open(title, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+
+def load_data(title):
+    with open(title, encoding='utf-8') as f:
+        return json.load(f)
+
+
+save_data('disney_data.json', movie_info_list)
